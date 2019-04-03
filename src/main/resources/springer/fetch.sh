9b6c4213ec506a6c2b855bd28e4b38eb6a879d5d
Asgeir Nilsen <asgeir@fintlabs.no>
2019-04-03 11:06:34 +0200

    Updated fetcher, adding more default data.

--- src/main/resources/springer/fetch.sh
+++ src/main/resources/springer/fetch.sh
@@ -7,4 +7,4 @@ env=play-with-fint
 # curl -s https://play-with-fint.felleskomponent.no/administrasjon/personal/arbeidsforhold | jq '._class = "no.fint.model.resource.administrasjon.personal.ArbeidsforholdResource"' > arbeidsforhold.json
 # curl -s https://play-with-fint.felleskomponent.no/administrasjon/organisasjon/organisasjonselement | jq '._class = "no.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource"' > organisasjonselement.json
 
-curl -s https://${env}.felleskomponent.no/$1 | sed -e 's#https://'${env}'.felleskomponent.no#${}#g' | jq '._class = "no.fint.model.resource.'$2'Resource"'
+curl -s https://${env}.felleskomponent.no/$1 | sed -e 's#https://'${env}'.felleskomponent.no/\([^/]*\)/\([^/]*\)/\([^/]*\)/#${\1.\2.\3}/#g' | jq '._class = "no.fint.model.resource.'$2'Resource"'
