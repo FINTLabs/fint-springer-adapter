bf06478430a0052405f29611b9af7615fb443ae1
Asgeir Nilsen <asgeir@fintlabs.no>
2019-10-23 17:26:47 +0200

    Refactored and added support for update conflicts for Variabellonn. And Fasttillegg.

--- src/main/java/no/fint/provider/springer/model/FastlonnRepository.java
+++ src/main/java/no/fint/provider/springer/model/FastlonnRepository.java
@@ -7,30 +7,23 @@ import no.fint.event.model.Operation;
 import no.fint.event.model.ResponseStatus;
 import no.fint.event.model.Status;
 import no.fint.model.administrasjon.personal.PersonalActions;
-import no.fint.model.felles.kompleksedatatyper.Identifikator;
 import no.fint.model.resource.FintLinks;
 import no.fint.model.resource.administrasjon.personal.FastlonnResource;
 import no.fint.provider.springer.behaviour.Behaviour;
-import no.fint.provider.springer.service.Handler;
 import no.fint.provider.springer.service.IdentifikatorFactory;
-import no.fint.provider.springer.storage.Springer;
-import no.fint.provider.springer.storage.Wrapper;
-import org.apache.commons.lang3.StringUtils;
+import no.fint.provider.springer.storage.LonnRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.util.StreamUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 @Repository
-public class FastlonnRepository implements Handler {
+public class FastlonnRepository extends LonnRepository {
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -40,12 +33,6 @@ public class FastlonnRepository implements Handler {
 
     @Autowired
     private List<Behaviour<FastlonnResource>> behaviours;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Autowired
-    private Wrapper wrapper;
 
     @Override
     public Set<String> actions() {
@@ -61,7 +48,7 @@ public class FastlonnRepository implements Handler {
                 case UPDATE_FASTLONN:
                     List<FastlonnResource> data = objectMapper.convertValue(response.getData(), objectMapper.getTypeFactory().constructCollectionType(List.class, FastlonnResource.class));
                     log.trace("Converted data: {}", data);
-                    final List<FastlonnResource> conflicts = findConflicts(data);
+                    final List<FastlonnResource> conflicts = findConflicts(data, FastlonnResource.class);
                     if (!conflicts.isEmpty() && response.getOperation() == Operation.CREATE) {
                         response.setStatus(Status.ADAPTER_REJECTED);
                         response.setResponseStatus(ResponseStatus.CONFLICT);
@@ -88,23 +75,6 @@ public class FastlonnRepository implements Handler {
             response.setResponseStatus(ResponseStatus.ERROR);
             response.setMessage(e.getMessage());
         }
-    }
-
-    private List<FastlonnResource> findConflicts(List<FastlonnResource> data) {
-        Set<String> sourceSystemIds = data.stream()
-                .filter(Objects::nonNull)
-                .map(FastlonnResource::getKildesystemId)
-                .filter(Objects::nonNull)
-                .map(Identifikator::getIdentifikatorverdi)
-                .filter(StringUtils::isNotBlank)
-                .collect(Collectors.toSet());
-        return StreamUtils
-                .createStreamFromIterator(mongoTemplate.stream(wrapper.query(FastlonnResource.class), Springer.class))
-                .map(wrapper.unwrapper(FastlonnResource.class))
-                .filter(r -> r.getKildesystemId() != null
-                        && StringUtils.isNotBlank(r.getKildesystemId().getIdentifikatorverdi())
-                        && sourceSystemIds.contains(r.getKildesystemId().getIdentifikatorverdi()))
-                .collect(Collectors.toList());
     }
 
 }
