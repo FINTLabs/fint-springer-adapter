2aa8caf3118e6f4e4c63c0a545522dd0eb8796d7
Asgeir Nilsen <asgeir@fintlabs.no>
2020-02-21 15:40:03 +0100

    Create UpdateSkoleressursHandler.java

---
+++ src/main/java/no/fint/provider/springer/handlers/UpdateSkoleressursHandler.java
@@ -0,0 +1,21 @@
+package no.fint.provider.springer.handlers;
+
+import no.fint.model.resource.utdanning.elev.SkoleressursResource;
+import no.fint.model.utdanning.elev.ElevActions;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Service
+public class UpdateSkoleressursHandler extends UpdateHandler<SkoleressursResource> {
+
+    public UpdateSkoleressursHandler() {
+        super(SkoleressursResource.class);
+    }
+
+    @Override
+    public Set<String> actions() {
+        return Collections.singleton(ElevActions.UPDATE_SKOLERESSURS.name());
+    }
+}
