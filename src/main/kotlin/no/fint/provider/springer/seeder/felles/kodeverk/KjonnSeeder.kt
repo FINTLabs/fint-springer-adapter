70aec1a9f9961f1f1d530a3c95d10d56b5abbcf5
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2026-03-05 14:52:54 +0100

    Refactor KjonnSeeder

--- src/main/kotlin/no/fint/provider/springer/seeder/felles/kodeverk/KjonnSeeder.kt
+++ src/main/kotlin/no/fint/provider/springer/seeder/felles/kodeverk/KjonnSeeder.kt
@@ -3,7 +3,6 @@ package no.fint.provider.springer.seeder.felles.kodeverk
 import no.fint.provider.springer.seeder.BaseSeeder
 import no.fint.provider.springer.storage.SeederRepository
 import no.novari.fint.model.felles.kompleksedatatyper.Identifikator
-import no.novari.fint.model.resource.Link
 import no.novari.fint.model.resource.felles.kodeverk.iso.KjonnResource
 import org.springframework.stereotype.Service
 
@@ -14,57 +13,24 @@ class KjonnSeeder(
 
     fun generateEntitiesForTest(): List<KjonnResource> = generateEntities()
 
+    private val kjonnData = listOf(
+        "5" to "transkjønnet mann",
+        "9" to "gjelder ikke",
+        "0" to "uvisst",
+        "4" to "transkjønnet kvinne",
+        "1" to "mann",
+        "2" to "kvinne",
+        "8" to "ikke oppgitt"
+    )
 
     override fun generateEntities(): List<KjonnResource> {
-        return listOf(
+        return kjonnData.map { (kodeverdi, navnverdi) ->
             KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "5" }
-                kode = "5"
-                navn = "transkjønnet mann"
-                addSelf(link<KjonnResource>("5"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "9" }
-                kode = "9"
-                navn = "gjelder ikke"
-                addSelf(link<KjonnResource>("9"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "0" }
-                kode = "0"
-                navn = "uvisst"
-                addSelf(link<KjonnResource>("0"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "4" }
-                kode = "4"
-                navn = "transkjønnet kvinne"
-                addSelf(link<KjonnResource>("4"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "1" }
-                kode = "1"
-                navn = "mann"
-                addSelf(link<KjonnResource>("1"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "2" }
-                kode = "2"
-                navn = "kvinne"
-                addSelf(link<KjonnResource>("2"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "8" }
-                kode = "8"
-                navn = "ikke oppgitt"
-                addSelf(link<KjonnResource>("8"))
-            },
-            KjonnResource().apply {
-                systemId = Identifikator().apply { identifikatorverdi = "7" }
-                kode = "7"
-                navn = "hermafroditt"
-                addSelf(link<KjonnResource>("7"))
+                systemId = Identifikator().apply { identifikatorverdi = kodeverdi }
+                kode = kodeverdi
+                navn = navnverdi
+                addSelf(link<KjonnResource>(kodeverdi))
             }
-        )
+        }
     }
 }
