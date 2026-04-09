2c26d95a236bdb8c6c7ffb3004874721630e6945
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-04-09 12:30:23 +0200

    update skoleresurs whit complete links

--- src/main/kotlin/no/fint/provider/springer/seeder/utdanning/utdanningsprogram/SkoleSeeder.kt
+++ src/main/kotlin/no/fint/provider/springer/seeder/utdanning/utdanningsprogram/SkoleSeeder.kt
@@ -6,8 +6,14 @@ import no.novari.fint.model.felles.kompleksedatatyper.Identifikator
 import no.novari.fint.model.felles.kompleksedatatyper.Kontaktinformasjon
 import no.novari.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource
 import no.novari.fint.model.resource.felles.kompleksedatatyper.AdresseResource
+import no.novari.fint.model.resource.utdanning.elev.ElevforholdResource
+import no.novari.fint.model.resource.utdanning.elev.KlasseResource
+import no.novari.fint.model.resource.utdanning.elev.KontaktlarergruppeResource
+import no.novari.fint.model.resource.utdanning.elev.SkoleressursResource
+import no.novari.fint.model.resource.utdanning.elev.UndervisningsforholdResource
 import no.novari.fint.model.resource.utdanning.timeplan.FagResource
 import no.novari.fint.model.resource.utdanning.utdanningsprogram.SkoleResource
+import no.novari.fint.model.utdanning.timeplan.Undervisningsgruppe
 import org.springframework.stereotype.Service
 
 @Service
@@ -50,6 +56,7 @@ class SkoleSeeder(
                 addSelf(link<SkoleResource>("XX1234"))
                 addOrganisasjon(link<OrganisasjonselementResource>("970123458", "organisasjonsnummer"))
                 addFag(link<FagResource>("FA_NOR1Z41_2018"))
+                addKlasse(link<KlasseResource>("BG_1STA-2018"))
             },
             SkoleResource().apply {
                 systemId = Identifikator().apply { identifikatorverdi = "1579" }
@@ -62,6 +69,21 @@ class SkoleSeeder(
                 addSelf(link<SkoleResource>("1579"))
                 addOrganisasjon(link<OrganisasjonselementResource>("313131313", "organisasjonsnummer"))
                 addFag(link<FagResource>("NOR1264"))
+                addKlasse(link<KlasseResource>("1321121"))
+                addKlasse(link<KlasseResource>("1321122"))
+                addKlasse(link<KlasseResource>("1321123"))
+                addElevforhold(link<ElevforholdResource>("500011-1"))
+                addElevforhold(link<ElevforholdResource>("500012-1"))
+                addElevforhold(link<ElevforholdResource>("500013-1"))
+                addElevforhold(link<ElevforholdResource>("500014-1"))
+                addKontaktlarergruppe(link<KontaktlarergruppeResource>("1321122_1IDA"))
+                addKontaktlarergruppe(link<KontaktlarergruppeResource>("1321122_2IDA"))
+                addSkoleressurs(link<SkoleressursResource>("9b0205c3-0cba-485a-ac32-dba70500fe55"))
+                addSkoleressurs(link<SkoleressursResource>("d9a7860f-cb4b-4198-a9d9-840399fcddec"))
+                addUndervisningsforhold(link<UndervisningsforholdResource>("7024200-93838-0-1-20211001000000"))
+                addUndervisningsforhold(link<UndervisningsforholdResource>("664b9b6b-8b1e-439d-87b3-82e0fedbbc7c"))
+                addUndervisningsgruppe(link<Undervisningsgruppe>("10128458_1IDA"))
+                addUndervisningsgruppe(link<Undervisningsgruppe>("10128458_2IDA"))
             }
         )
     }
